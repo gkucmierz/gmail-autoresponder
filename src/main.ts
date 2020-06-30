775a1500b56647928e6a5c94098d9025f44cab5d
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-06-30 16:33:56 +0200

    reorganize code; add autosignin option

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,61 @@ import { getGapi } from 'gapi-browser';
 
 const EventEmitter = require('events');
 
-const GmailAddon = (CLIENT_ID: string, API_KEY: string) => {
+const GmailAutoresponder = (CLIENT_ID: string, API_KEY: string = '') => {
   const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'];
   const SCOPES = 'https://www.googleapis.com/auth/gmail.settings.basic';
 
   const eventEmitter = new EventEmitter();
   let gapi: any;
 
-  const updateSigninStatus = (status: boolean) => {
+  const updateSignInStatus = (status: boolean) => {
     const event = status ? 'signIn' : 'signOut';
     eventEmitter.emit(event, status);
   };
+
+  const loadPromise = new Promise((resolve, reject) => {
+    getGapi.then((gapiRef: any) => {
+      gapi = gapiRef;
+      gapi.load('client:auth2', () => {
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(() => {
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+          updateSignInStatus(isSignedInSync());
+          resolve();
+        }, (error: any) => reject(error));
+      }, (error: any) => reject(error));
+    });
+  });
+
+  const syncMethod = (fn: Function) => {
+    let loaded = false;
+    loadPromise.then(() => loaded = true);
+    return (...args: any[]) => {
+      if (loaded) return fn(args);
+      return false;
+    };
+  };
+
+  const asyncMethod = (fn: Function) => {
+    return (...args: any[]) => loadPromise.then(() => fn(...args));
+  };
+
+  const requireSignin = (fn: Function) => (...args: any[]) => {
+    return isSignedIn().then(signedIn => {
+      return signedIn ? fn(...args) : signIn().then(() => fn(...args));
+    });
+  };
+
+  const isSignedInSync = () => {
+    if (!gapi || !gapi.auth2) return false;
+    return gapi.auth2.getAuthInstance().isSignedIn.get();
+  };
+
+  const isSignedIn = asyncMethod(() => new Promise(resolve => resolve(isSignedInSync())));
 
   const setVacationSetting = (config: any) => new Promise((resolve, reject) => {
     gapi.client.gmail.users.settings.updateVacation({userId: 'me'}, config).execute((res: any) => {
@@ -25,17 +69,27 @@ const GmailAddon = (CLIENT_ID: string, API_KEY: string) => {
     });
   });
 
-  const isSignedIn = () => gapi.auth2.getAuthInstance().isSignedIn.get();
-  const setAutoresponder = (message: string, topic = '', restrictToContacts = false) => {
+  const setAutoresponder = asyncMethod((message: string, topic = '', restrictToContacts = false) => {
     return setVacationSetting({
       enableAutoReply: true,
       responseSubject: topic,
       responseBodyHtml: message,
       restrictToContacts,
     });
-  };
-  const resetAutoresponder = () => setVacationSetting({ enableAutoReply: false });
-  const signIn = () => new Promise((resolve, reject) => {
+  });
+
+  const resetAutoresponder = asyncMethod(() => setVacationSetting({ enableAutoReply: false }));
+  const getAutoresponder = asyncMethod(() => new Promise((resolve, reject) => {
+    gapi.client.gmail.users.settings.getVacation({userId: 'me'}).execute((res: any) => {
+      if ('error' in res) {
+        reject(res.error);
+      } else {
+        resolve(res);
+      }
+    });
+  }));
+
+  const signIn = asyncMethod(() => new Promise((resolve, reject) => {
     const DELAY = 50;
     const signInObj = gapi.auth2.getAuthInstance().signIn();
     const statusProp = 'Da';
@@ -50,72 +104,31 @@ const GmailAddon = (CLIENT_ID: string, API_KEY: string) => {
         setTimeout(loop, DELAY);
       }
     })();
-  });
-  const signOut = () => new Promise((resolve, reject) => {
+  }));
+
+  const signOut = asyncMethod(() => new Promise((resolve, reject) => {
     try {
       gapi.auth2.getAuthInstance().signOut();
       resolve(); 
     } catch(error) {
       reject(error);
     }
-  });
-  const getAutoresponder = () => new Promise((resolve, reject) => {
-    gapi.client.gmail.users.settings.getVacation({userId: 'me'}).execute((res: any) => {
-      if ('error' in res) {
-        reject(res.error);
-      } else {
-        resolve(res);
-      }
-    });
-  });
-  const getUserInfo = () => {
-    if (!isSignedIn()) return false;
-    const props: any = {
-      fullname: 'Bd',
-      email: 'Du',
-      lastname: 'FU',
-      name: 'FW',
-      image: 'hL',
-    };
-    const res: any = {};
-    const userInfo: any = gapi.auth2.getAuthInstance().currentUser.ke.Tt;
-    Object.keys(props).map((prop: string) => res[prop] = userInfo[props[prop]]);
-    return res;
-  };
-
-  new Promise((resolve, reject) => {
-    getGapi.then((gapiRef: any) => {
-      gapi = gapiRef;
-      gapi.load('client:auth2', () => {
-        gapi.client.init({
-          apiKey: API_KEY,
-          clientId: CLIENT_ID,
-          discoveryDocs: DISCOVERY_DOCS,
-          scope: SCOPES
-        }).then(() => {
-          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-          updateSigninStatus(isSignedIn());
-          resolve();
-        }, (error: any) => {
-          reject(error);
-        });
-      });
-    });
-  });
+  }));
 
   return {
+    isSignedInSync: syncMethod(isSignedInSync),
     isSignedIn,
-    setAutoresponder,
-    resetAutoresponder,
-    getAutoresponder,
+    setAutoresponder: requireSignin(setAutoresponder),
+    resetAutoresponder: requireSignin(resetAutoresponder),
+    getAutoresponder: requireSignin(getAutoresponder),
+    getAutoresponder2: getAutoresponder,
     signIn,
     signOut,
-    getUserInfo,
     eventEmitter,
   };
 };
 
 export {
-  GmailAddon,
+  GmailAutoresponder,
   getGapi,
 }
